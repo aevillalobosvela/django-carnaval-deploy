98ce5d93c4120cc9d5828f4d93f3f61fa8054014
Alvaro Villalobos Vela <vivealed@gmail.com>
2025-04-21 01:22:44 -0400

    update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
    'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
+        conn_max_age=600s
     )
 } """
 
