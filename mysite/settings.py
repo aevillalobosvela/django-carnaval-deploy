0722e9f7533719225574e2490b816b1a3c127922
Alvaro Villalobos Vela <vivealed@gmail.com>
2025-04-21 01:10:19 -0400

    update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 } """
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / "db.sqlite3",
-   }
+   'default': dj_database_url.config(
+       default='postgresql://postgres@localhost/postgres',
+       conn_max_age=600
+   )
 }
 
 # Password validation
